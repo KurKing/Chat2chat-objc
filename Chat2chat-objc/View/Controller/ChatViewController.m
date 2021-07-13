e0672d2c645e896eb483e0dd2510ac5798026a91
KurKing <lyosha.kurkin@gmail.com>
2021-07-13 16:22:59 +0300

    Create a method which gets messages info from ViewModel

--- Chat2chat-objc/View/Controller/ChatViewController.m
+++ Chat2chat-objc/View/Controller/ChatViewController.m
@@ -8,7 +8,9 @@
 #import "ChatViewController.h"
 #import "SelfMessageTableViewCell.h"
 #import "InterlocutorMessageTableViewCell.h"
+#import "MessageTableViewCell.h"
 #import "ChatViewModel.h"
+#import "Message.h"
 
 @interface ChatViewController () <UITableViewDelegate, UITableViewDataSource>
 
@@ -20,24 +22,24 @@
 
 @property (strong, nonatomic) IBOutlet ChatViewModel *viewModel;
 
-
-@property (strong, nonatomic) NSArray *messages;
-
 @end
 
 @implementation ChatViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-#warning TODO mock data
-    self.messages = @[
-        @"1",@"12345",
-        @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    ];
-    [self.viewModel setupWithController:self];
     
-    // table view preparing
+    [self.viewModel setupWithController:self];
+    [self setUpTableView];
+    [self setUpKeyboard];
+}
+
+- (IBAction)sendMessageButtonPressed {
+    NSLog(@"sendMessageButtonPressed");
+}
+
+#pragma mark UITableView
+- (void) setUpTableView {
     self.messagesTableView.dataSource = self;
     self.messagesTableView.delegate = self;
     self.messagesTableView.tableFooterView = [UIView new];
@@ -45,8 +47,31 @@
     [self.messagesTableView registerNib:[UINib nibWithNibName: SelfMessageTableViewCell.identifier bundle: nil] forCellReuseIdentifier: SelfMessageTableViewCell.identifier];
     
     [self.messagesTableView registerNib:[UINib nibWithNibName: InterlocutorMessageTableViewCell.identifier bundle: nil] forCellReuseIdentifier: InterlocutorMessageTableViewCell.identifier];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 6;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+        
+    Message *message = [self.viewModel getMessageWithIndex:indexPath.row];
     
-    // keyboard hiding
+    MessageTableViewCell *cell;
+    
+    if (message.type == MyMessage) {
+        cell = [self.messagesTableView dequeueReusableCellWithIdentifier: SelfMessageTableViewCell.identifier];
+    } else {
+        cell = [self.messagesTableView dequeueReusableCellWithIdentifier: InterlocutorMessageTableViewCell.identifier];
+    }
+
+    [cell setMessageText: message.text];
+    
+    return cell;
+}
+
+#pragma mark Keyboard
+- (void) setUpKeyboard {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     
@@ -55,31 +80,6 @@
     [self.messagesTableView addGestureRecognizer: tap];
 }
 
-- (IBAction)sendMessageButtonPressed {
-    NSLog(@"sendMessageButtonPressed");
-}
-
-#pragma mark UITableViewDataSource
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 6;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if (indexPath.row < 3) {
-        SelfMessageTableViewCell *cell = [self.messagesTableView dequeueReusableCellWithIdentifier: SelfMessageTableViewCell.identifier];
-        
-        [cell setMessageText: self.messages[indexPath.row]];
-        return  cell;
-    }
-    
-    InterlocutorMessageTableViewCell *cell = [self.messagesTableView dequeueReusableCellWithIdentifier: InterlocutorMessageTableViewCell.identifier];
-    
-    [cell setMessageText: self.messages[indexPath.row-3]];
-    return cell;
-}
-
-#pragma mark Keyboard
 - (void) keyboardWillShow:(NSNotification *) notification {
     NSValue *keyboardSize = notification.userInfo[UIKeyboardFrameEndUserInfoKey];
     if (keyboardSize == nil) { return; }
@@ -88,9 +88,10 @@
     self.bottomConstraintForChatView.constant = heigth;
     [self.view layoutIfNeeded];
     
-#warning TODO: row counting
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:5 inSection:0];
-    [self.messagesTableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    if (!self.viewModel.isMessagesEmpty) {
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:(self.viewModel.messagesCount-1) inSection:0];
+        [self.messagesTableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    }
 }
 
 - (void) keyboardWillHide:(NSNotification *) notification {
@@ -98,7 +99,7 @@
     [self.view layoutIfNeeded];
 }
 
--(void) dissmissKeyboard:(NSNotification *) notification {
+- (void) dissmissKeyboard:(NSNotification *) notification {
     [self.view endEditing:YES];
 }
 
